0ceefc4e76726f68695f290188f5fb85def2d11b
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-02-01 16:42:13 +0100

    Add componente AppTitle e prove stilizzazione

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script>
+import AppTitle from  "./components/AppTitle.vue"
+
 export default {
+  components: {
+    AppTitle
+  },
   data() {
     return {
       count: 0
@@ -9,11 +14,23 @@ export default {
 </script>
 
 <template>
-  <button @click="count++">Count is: {{ count }}</button>
+  <div class="background">
+    <AppTitle/>
+
+    <button @click="count++">Count is: {{ count }}</button>
+  </div>
 </template>
 
 <style scoped>
+.background {
+  width: 100vw;
+  height: 100vh;
+  background-color: cornflowerblue;
+  padding-top: 5rem;
+}
+
 button {
   font-weight: bold;
+  margin-top: 2rem;
 }
 </style>
